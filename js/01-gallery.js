93394ec2ff4b2bb59df9125a5b013a2cc70dbccb
PelageyaTelezhynska <p.yu.telezhynska@gmail.com>
2022-09-27 17:46:18 +0300

    add lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,33 +1,41 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+
+
 const refs = {
   gallery: document.querySelector(".gallery"),
 };
 
 console.log(galleryItems);
-const markup = galleryItems
+
+
+  const markup = galleryItems
   .map(
     ({ original, preview, description }) =>
-      `<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a>`
+      `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
   )
   .join("");
 
+function onGalleryClick(event) {
+event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) return;
+  
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`);
+  
+  instance.show(); 
+}
+
+window.addEventListener('keydown', onEscapeClick)
+function onEscapeClick(event) {
+  if (event.code === "Escape") { instance.close() };
+}
+
 refs.gallery.insertAdjacentHTML("beforeend", markup);
 
-const originImgLink = gallery.dataset.source;
+refs.gallery.addEventListener("click", onGalleryClick);
 
-const instance = basicLightbox.create(`
-    <img src="${originImgLink}" width="1280">
-`);
 
-refs.gallery.addEventListener("click", onItem);
 
-function onItem(event) {
-  preventDefault();
-  if (event.target === event.currentTarget) {
-    return;
-  }
-
-  instance.show();
-}
